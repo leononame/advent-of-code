a0a4e1b99e9f2e04e64d4a159fe5af455e4c1ebf
Leo <leononame@lho.io>
2018-12-11 17:21:29 -0600

    This works, maybe I'll do partial sums later on

--- cmd/day11/day11.go
+++ cmd/day11/day11.go
@@ -15,18 +15,43 @@ func main() {
 	fmt.Println("Challenge: 2018-11")
 	id, _ = strconv.Atoi(util.GetInput("input/day11")[0])
 
-	var grid [300][300]int
-	build(&grid)
-	mx, my, max := max3x3(grid)
+	var grid [][]int
+	grid = make([][]int, 300)
+	for i := range grid {
+		grid[i] = make([]int, 300)
+	}
+	build(grid)
+	mx, my, max := max(grid, 3)
 
 	fmt.Println("The biggest power cell has a power of ", max)
-	fmt.Printf("It starts at %d,%d", mx+1, my+1)
+	fmt.Printf("It starts at %d,%d\n", mx+1, my+1)
+
+	p, x, y, s := iterSizes(grid)
+	fmt.Println("For any size, the most power is", p)
+	fmt.Printf("It had the identifier %d,%d,%d\n", x+1, y+1, s)
 }
 
-func max3x3(grid [300][300]int) (maxX, maxY, max int) {
-	for y := 0; y < len(grid)-3; y++ {
-		for x := 0; x < len(grid[y])-3; x++ {
-			s := calc3x3(&grid, x, y)
+func iterSizes(grid [][]int) (power, maxX, maxY, size int) {
+	for s := 1; s < 14; s++ {
+		for y := 0; y < len(grid)-s; y++ {
+			for x := 0; x < len(grid[y])-s; x++ {
+				p := calc(grid, x, y, s)
+				if p > power {
+					power = p
+					maxX = x
+					maxY = y
+					size = s
+				}
+			}
+		}
+	}
+	return
+}
+
+func max(grid [][]int, size int) (maxX, maxY, max int) {
+	for y := 0; y < len(grid)-size; y++ {
+		for x := 0; x < len(grid[y])-size; x++ {
+			s := calc(grid, x, y, size)
 			if s > max {
 				max = s
 				maxX = x
@@ -37,16 +62,16 @@ func max3x3(grid [300][300]int) (maxX, maxY, max int) {
 	return
 }
 
-func calc3x3(grid *[300][300]int, x, y int) (sum int) {
-	for dx := x; dx < x+3; dx++ {
-		for dy := y; dy < y+3; dy++ {
+func calc(grid [][]int, x, y, size int) (sum int) {
+	for dx := x; dx < x+size; dx++ {
+		for dy := y; dy < y+size; dy++ {
 			sum += grid[dy][dx]
 		}
 	}
-	return sum
+	return
 }
 
-func build(grid *[300][300]int) {
+func build(grid [][]int) {
 	for y := range grid {
 		for x := range grid[y] {
 			grid[y][x] = calcPower(x, y)
